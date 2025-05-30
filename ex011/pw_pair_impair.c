1e4b7ba44a9a242d71b869beba6abc9cf64945f7
root <root@DESKTOP-43QCTFI.localdomain>
2025-05-30 12:01:02 +0200

    Pair ou Impair

---
+++ ex011/pw_pair_impair.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+char pw_pair_impair(char *str){
+
+    if (str == NULL) {
+        return 'N'; // Return 0 if the input string is null
+    } // Return 0 if the input string is null
+
+
+
+   int count =0;
+   char *alpahbet = str;
+
+    while (*alpahbet != '\0'){
+        if((*alpahbet >= 'A' && *alpahbet <= 'Z') || (*alpahbet >= 'a' && *alpahbet <= 'z')){ 
+            count++;  /* code */
+        }
+        alpahbet++;
+    }
+    return (count % 2 == 0) ? 'P' : 'I';
+
+}
+
+
+
+
+
+
